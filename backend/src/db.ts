3b7579d1c47f47abeda92f059cd45e1ae9533600
rohancodescs <74475800+rohancodescs@users.noreply.github.com>
2025-05-05 03:12:06 -0400

    enabling ssl

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,2 +1,12 @@
-import pg from "pg";
-export const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL! });
+import pkg from 'pg';
+const { Pool } = pkg;
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,  // Supabase self-signed cert
+  },
+});
+
+// import pg from "pg";
+// export const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL! });
