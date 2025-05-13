b480db79f1022ea4a95bdd30cffd0def069563df
Rohan Bhatt <74475800+rohancodescs@users.noreply.github.com>
2025-05-13 00:27:15 -0400

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -46,3 +46,52 @@ if (require.main === module) {
     console.log(`API ready at http://localhost:${process.env.PORT || 4000}`)
   );
 }
+
+// import express, { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
+// import cors from 'cors';
+// import guestRoutes from './routes/guests';
+// import { ZodError } from 'zod';
+// import { ParseError } from 'libphonenumber-js';
+
+// export const app = express();
+// // app.use(cors());
+// const allowed = [
+//   'http://localhost:3000',
+//   /^https:\/\/raft-challenge.*\.vercel\.app$/   // production + preview URLs
+// ];
+// app.use(
+//   cors({
+//     origin: allowed,
+//     credentials: true       
+//   })
+// );
+// app.get('/', (_req: Request, res: Response) => {
+//   res.send('Guestbook API OK');
+// });
+// app.use(express.json());
+// app.use("/api/guests", guestRoutes);
+// // app.use('/api/guests', guestRoutes);
+
+// /* ---------- error handler ---------- */
+// const apiErrorHandler: ErrorRequestHandler = (
+//   err: unknown,
+//   _req: Request,
+//   res: Response,
+//   _next: NextFunction
+// ) => {
+//   if (err instanceof ZodError || err instanceof ParseError) {
+//     res.status(400).json({ message: err.message });
+//     return ;
+//   }
+//   console.error(err);
+//   res.status(500).json({ message: 'Internal Server Error' });
+// };
+
+// app.use(apiErrorHandler);
+
+// /* ---------- listener (skip in tests) ---------- */
+// if (require.main === module) {
+//   app.listen(process.env.PORT || 4000, () =>
+//     console.log(`API ready at http://localhost:${process.env.PORT || 4000}`)
+//   );
+// }
